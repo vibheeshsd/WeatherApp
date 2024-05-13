cf3fe379fd393be65ea789ce3b71264a263672d4
vibheeshsd <vibheesh281.pec@gmail.com>
2024-05-13 19:26:36 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text className = "text-red">Open up App.js121</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
